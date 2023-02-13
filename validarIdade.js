9d7f3aa29b33bed087b14afd7eafdca808c8f1cf
Demarque <demarque.ads@gmail.com>
2023-02-13 14:23:23 -0300

    Script para validar idade

---
+++ validarIdade.js
@@ -0,0 +1,11 @@
+const today = new Date();
+const birthDate = new Date(input);
+const y = today.getFullYear() - birthDate.getFullYear();
+const m = today.getMonth() - birthDate.getMonth();
+const d = today.getDate() - birthDate.getDate();
+if (m < 0 || (m === 0 && d < 0)) {
+    y--;
+};
+if (y < 18) {
+    return ('The user is minor')
+}
